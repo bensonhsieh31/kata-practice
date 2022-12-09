cb8002500a9b90834afd57a7a12cbfd9fdae5cfe
bensonhsieh <bensonhsieh31@gmail.com>
2022-12-09 09:56:59 +0800

    test: add item_foo_update_quality_success.

--- src/test/java/com/kata/demo/GildedRoseTest.java
+++ src/test/java/com/kata/demo/GildedRoseTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 class GildedRoseTest {
 
 	@Test
-	void foo() {
+	void item_foo_update_quality_success() {
 		Item[] items = new Item[] { new Item("foo", 0, 0) };
+		Item expectedItem = items[0];
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
-		assertEquals("fixme", app.items[0].name);
+		Item actualItem = app.items[0];
+		assertEquals(expectedItem.name, actualItem.name);
 	}
 
 }
