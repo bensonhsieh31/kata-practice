9c8bd0891272d2241f5e995690d418d15532814b
bensonhsieh <bensonhsieh31@gmail.com>
2022-12-12 23:08:22 +0800

    test: modify item_foo_update_quality_success

--- src/test/java/com/kata/demo/GildedRoseTest.java
+++ src/test/java/com/kata/demo/GildedRoseTest.java
@@ -2,18 +2,22 @@ package com.kata.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class GildedRoseTest {
 
-	@Test
-	void item_foo_update_quality_success() {
-		Item[] items = new Item[] { new Item("foo", 0, 0) };
-		Item expectedItem = items[0];
+	@ParameterizedTest
+	@CsvSource(value = { 
+			"10; 9",
+			"0; 0",
+			}, delimiter = ';')
+	void item_foo_update_quality_success(int itemQuality, int expectedQuality) {
+		Item[] items = new Item[] { new Item("foo", 3, itemQuality) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
 		Item actualItem = app.items[0];
-		assertEquals(expectedItem.name, actualItem.name);
+		assertEquals(expectedQuality, actualItem.quality);
 	}
 
 }
