8b2d42abcdc31b34ade56a83266e29b445a81d18
bensonhsieh <bensonhsieh31@gmail.com>
2023-01-03 22:47:26 +0800

    pass test display_number_one_lcd_style

--- src/main/java/com/kata/demo/lcd/LCDMonitor.java
+++ src/main/java/com/kata/demo/lcd/LCDMonitor.java
@@ -2,6 +2,9 @@ package com.kata.demo.lcd;
 
 public class LCDMonitor {
     public String displayLCDStyleNumbers(String numbers) {
+        if(numbers.equals("1")){
+            return "|\n|";
+        }
         return "";
     }
 }
